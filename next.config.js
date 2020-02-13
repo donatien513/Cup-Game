843b5546d8c7ea8bc3b1421f6eaf70b16944f14b
Donatien <donatiennambinintsoa@gmail.com>
2020-02-13 13:32:06 +0300

    Add assetsPrefix for GH page compatibility

--- next.config.js
+++ next.config.js
@@ -1,6 +1,24 @@
 /* eslint-disable */
 
+const url = require('url');
+const path = require('path');
+const ghpages = require('gh-pages');
+const packageJson = require('./package');
+const { CI } = process.env;
+
+/**
+ * Extract repo name from package.json
+ */
+const getRepositoryName = () => {
+  const repoUrl = packageJson.repository.url;
+  const parsedRepoURL = url.parse(repoUrl);
+  const extname = path.extname(parsedRepoURL.pathname);
+  return path.basename(parsedRepoURL.pathname).replace(extname, '');
+}
+
+// next.config.js
 const withSass = require('@zeit/next-sass')
 module.exports = withSass({
+  assetPrefix: CI ? `/${getRepositoryName()}` : '',
   /* config options here */
 })
